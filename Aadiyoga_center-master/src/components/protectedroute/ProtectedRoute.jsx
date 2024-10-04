6908a1f09a40e803c59b6697761cac4851efbe06
UMAR MUKHTAR SHAIKH <108225144+Umar9823@users.noreply.github.com>
2024-10-04 11:05:45 +0530

    Update ProtectedRoute.jsx

--- Aadiyoga_center-master/src/components/protectedroute/ProtectedRoute.jsx
+++ Aadiyoga_center-master/src/components/protectedroute/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-// src/components/protectedroute/ProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext'; // Adjust the import path if needed
 
-const ProtectedRoute = ({ element }) => {
+const ProtectedRoute = ({ children }) => {
     const { user } = useAuth();
 
-    return user ? element : <Navigate to="/signin" />;
+    // If user is logged in, render children, otherwise redirect to "/signin"
+    return user ? children : <Navigate to="/signin" />;
 };
 
 export default ProtectedRoute;
